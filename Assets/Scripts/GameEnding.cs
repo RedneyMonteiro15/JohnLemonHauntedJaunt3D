964b26c98e2dc74e1f38ef323e802690e0c2967d
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2022-05-01 23:07:40 +0100

    10.Start the GameEnding Script

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+
+        }
+    }
+}
