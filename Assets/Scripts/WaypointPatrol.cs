665d70cd7c55cbdb7c60559f57fea9a4ffef9e76
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2022-05-02 19:03:44 +0100

    8.Set the Destination of the Nav Mesh Agent

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class WaypointPatrol : MonoBehaviour
 {
+    public Transform[] waypoints;
+    public NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent.SetDestination(waypoints[0].position);
     }
 
     // Update is called once per frame
