3888568c45e6cf5928c62ae22bff3587b31b1c43
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2022-05-02 19:05:29 +0100

    10. Adicione Destinos Adicionais

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -7,16 +7,20 @@ using UnityEngine.AI;
 public class WaypointPatrol : MonoBehaviour
 {
     public Transform[] waypoints;
+    int m_CurrentWaypointIndex;
     public NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
-        navMeshAgent.SetDestination(waypoints[0].position);
+        navMeshAgent.SetDestination (waypoints[0].position);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination (waypoints[m_CurrentWaypointIndex].position);
+        }
     }
 }
